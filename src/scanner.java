d91946f95ca5efdd1ce7a4ae6729c95671a7284f
pidsonn <muturanepidson@gmail.com>
2024-09-27 22:17:09 +0300

    Create scanner.java

---
+++ src/scanner.java
@@ -0,0 +1,14 @@
+import java.io.InputStream;
+
+public class scanner {
+
+    public scanner(InputStream in) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public double nextDouble() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextDouble'");
+    }
+
+}
